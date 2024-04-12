7f1b514aec297d619b135772a2d5a9001d37b147
Gungun-Pathak <gungunpathak600@gmail.com>
2024-04-12 22:58:27 +0530

    commit2

--- client/negotiator/src/App.jsx
+++ client/negotiator/src/App.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Home.jsx";
-
+import Home from "./Pages/Home.jsx";
+import Product from "./Pages/Product.jsx";
+import Login from "./Pages/Login.jsx";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login/>} />
-      <Route path="/home" element={<Home/>} />
-      <Route path="/product" element={<Product/>} />
-      {/* <Route path="/home" element={<Home/>} /> */}
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+        {/* <Route path="/home" element={<Home/>} /> */}
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
